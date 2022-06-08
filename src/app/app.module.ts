887b577e4dcad45131082b24a005577630b6262f
Diego Celestino <diegocdsantos@gmail.com>
2022-06-08 16:42:23 -0300

    Aula 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +13,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
